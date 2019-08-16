d55c2206247659a374c3ead83f9dba2e0fd456aa
Chloe Mueller <chloemueller@chloemueller-mbp.local>
2019-08-16 14:01:44 -0400

    images set up

--- script.js
+++ script.js
@@ -1,2 +1,53 @@
-var me = document.getElementsByClassName("container")[0];
-console.log(window.getComputedStyle(me).getPropertyValue("margin-left"));
+
+
+
+const Main = (() => {
+
+    let container = document.getElementsByClassName("container")[0];
+    let SH = window.innerHeight;
+    let SW = window.innerWidth;
+
+    const _init = () => {
+        'use strict';
+        initWindowEvents();
+
+/*        var $slider = document.getElementById('shoe-col');
+        var $shoe = document.getElementById('shoeMarker');
+        var $book = document.getElementsByClassName('col-md-4')[2];
+        var $web = document.getElementsByClassName('col-md-4')[1];
+
+
+        $slider.addEventListener('click', function() {
+          var isOpen = $slider.classList.contains('slide-in');
+          if(isOpen){
+            $slider.classList.replace('slide-in', 'slide-out');
+            $book.style.display= "none";
+            $web.style.display= "none";
+          }
+
+
+          //$shoe.setAttribute('id', isOpen ? 'shoeMarker-slideout' : 'shoeMarker');
+        }); */
+
+
+    };
+
+
+
+
+
+    const initWindowEvents = () => {
+        window.addEventListener( 'resize', onWindowResize, false );
+    }
+
+    const onWindowResize = () => {
+
+    };
+
+    return {
+        init: _init,
+    }
+})();
+
+
+document.addEventListener('DOMContentLoaded', Main.init);
